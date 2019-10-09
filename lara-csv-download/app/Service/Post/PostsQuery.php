f9e3ae10da529f4b541e271b49cc2451cd98eaa1
n1215 <knakae1215@gmail.com>
2019-10-09 13:37:37 +0900

    不要なLazyCollection::make()を削除

--- lara-csv-download/app/Service/Post/PostsQuery.php
+++ lara-csv-download/app/Service/Post/PostsQuery.php
@@ -18,21 +18,26 @@ class PostsQuery
     private const CHUNK_COUNT = 100;
 
     /**
+     * @var LazyChunker
+     */
+    private $chunker;
+
+    public function __construct()
+    {
+        $this->chunker = new LazyChunker(self::CHUNK_COUNT, function (Collection $posts) {
+            $posts->load('author');
+            foreach($posts as $post) {
+                yield $post;
+            }
+        });
+    }
+
+    /**
      * @return Enumerable<Post>
      */
     public function get(): Enumerable
     {
-        return LazyCollection::make((function () {
-            $query = Post::query();
-
-            $chunker = (new LazyChunker(self::CHUNK_COUNT, function (Collection $posts) {
-                $posts->load('author');
-                foreach($posts as $post) {
-                    yield $post;
-                }
-            }));
-
-            return $chunker->chunk($query);
-        }));
+        $query = Post::query();
+        return $this->chunker->chunk($query);
     }
 }
